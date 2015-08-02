9e87d9c27cf8e457e4ace6f18231063c237e920b
iamist <vicentechristian@gmail.com>
2015-08-03 06:42:10 +0800

    update method declaration of base collection object

--- library/datastore/_base/collection.js
+++ library/datastore/_base/collection.js
@@ -1,32 +1,49 @@
 var getobject = require('getobject');
 var moment = require('moment');
+var _ = require('lodash');
 var Collection;
 
 Collection = function(name, connection) {
-    var _ = _.lodash.runInContext();
 
-    this._ = _;
+    //@TODO: add instance validation of connection param
+
     this.name = name;
     this.connection = connection;
 
-    this.find = function(criteria, opts, cb) {
-      return cb(_.find(this._data, criteria))
-    }
-
-    this.create = function(data, cb) {
-      return cb(err, result);
-    }
-
-    this.save = function(identifier, data, cb) {
-
-    };
-
-    this.destroy = function(identifier, cb) {
-      return cb(err, result);
-    }
-
     return this;
-    
+
 }
+
+Collection.prototype.find = function(criteria, opts, cb) {
+  var result;
+  result = this.connection.adapter.find(this, criteria, opts);
+  return cb(null, result);
+};
+
+Collection.prototype.create = function(data, cb) {
+  var result;
+  result = this.connection.adapter.create(this, data);
+  return cb(null, result);
+};
+
+Collection.prototype.save = function(identifier, data, cb) {
+  var result;
+  if (_.isEmpty(identifier)) {
+    return cb(new Error('Arg `identifier` is required.'), null);
+  }
+
+  result = this.connection.adapter.save(this, identifier, data);
+  return cb(null, result);
+};
+
+Collection.prototype.destroy = function(identifier, cb) {
+  var result;
+
+  if (_.isEmpty(identifier)) {
+    return cb(new Error('Arg `identifier` is required.'), null);
+  }
+
+  return cb(null, result);
+};
 
 module.exports = Collection(name, connection);
